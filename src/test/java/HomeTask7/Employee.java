bca1c075815e3222f4f3bbc8d1e007339dcd93e4
Urmil Gajera <urmil_gajera@epam.com>
2021-09-25 18:31:10 +0530

    module 7

---
+++ src/test/java/HomeTask7/Employee.java
@@ -0,0 +1,13 @@
+package HomeTask7;
+
+public class Employee {
+    String name;
+    String account;
+    int salary;
+    public Employee(String name,String account, int salary)
+    {
+        this.name=name;
+        this.account=account;
+        this.salary=salary;
+    }
+}
